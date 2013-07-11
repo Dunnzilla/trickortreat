3032501f888c13441c8f0359b819b64759f2b767
Jason Dunn <attnjd@gmail.com>
2013-07-10 22:14:07 -0500

    Make run script do something.

--- run_trick_or_treat.rb
+++ run_trick_or_treat.rb
@@ -2,21 +2,46 @@
 
 require './trick_or_treat'
 
-home = House.new("Home", 12)
-jason = TrickOrTreater.new("Jason", 38)
-rose = TrickOrTreater.new("Rose", 2)
 
-puts "Before knocking:\n"
-puts jason
-puts rose
-puts home
+def print_candy_counts(kids)
+  puts "----"
+  kids.each do |kid|
+    puts "#{kid.name} has #{kid.candies_in_bag} candies."
+  end
+end
+
+def run_demo
+  houses = [
+    House.new("Home", 100),
+    House.new("Cheap Neighbors", 10),
+    House.new("Show-off Neighbors", 500),
+    House.new("Weirdos Down the Street", 0),
+  ]
+
+  kids = [
+    TrickOrTreater.new("Billy", 12),
+    TrickOrTreater.new("Marina", 8),
+    TrickOrTreater.new("Dr. Horseface", 73),
+    TrickOrTreater.new("Little Timmy", 5),
+    TrickOrTreater.new("Cher", 5),
+  ]
+  puts "----"
+  puts "There are #{kids.count} costumed people trick-or-treating."
+  puts "There are #{houses.count} houses."
+
+  print_candy_counts(kids)
+
+  kids.each do |kid|
+    houses.each do |house|
+      kid.knock(house)
+    end
+  end
+
+  puts "\nAfter all the kids knock on all the doors..."
+
+  print_candy_counts(kids)
+end
 
 
-jason.knock(home)
-rose.knock(home)
+run_demo
 
-
-puts "\nAfter knocking:\n"
-puts jason
-puts rose
-puts home
